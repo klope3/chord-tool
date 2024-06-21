206766353d0b788ab3fb5a2375c2ec5afd4f43d8
Josh Klope <klope3@msn.com>
2024-06-20 21:40:11 -0500

    Add chord progression and scale examples

--- src/app/InstrumentPlayer.tsx
+++ src/app/InstrumentPlayer.tsx
@@ -26,14 +26,47 @@ export function InstrumentPlayer() {
   function clickPiano() {
     if (!piano) return;
 
-    piano.triggerAttackRelease("C4", "4n");
+    const now = Tone.now();
+    piano.triggerAttackRelease(["C4", "E4", "G4"], "1", now);
+    piano.triggerAttackRelease(["C4", "F4", "A4"], "1", now + 1);
+    piano.triggerAttackRelease(["C4", "E4", "G4"], "1", now + 2);
+    piano.triggerAttackRelease(["B3", "D4", "G4"], "1", now + 3);
+    piano.triggerAttackRelease(["C4", "E4", "G4"], "1", now + 4);
+  }
+
+  function clickScale() {
+    if (!piano) return;
+
+    const now = Tone.now();
+    const notes = [
+      "C4",
+      "D4",
+      "E4",
+      "F4",
+      "G4",
+      "A4",
+      "B4",
+      "C5",
+      "D5",
+      "E5",
+      "F5",
+      "G5",
+      "A5",
+      "B5",
+      "C6",
+    ];
+    for (let i = 0; i < notes.length; i++) {
+      piano.triggerAttackRelease(notes[i], "0.25", now + 0.25 * i);
+    }
+    piano.triggerAttackRelease("C4", "0.25", now);
   }
 
   return (
     <div>
       <button onClick={clickPiano} id="playPianoC">
-        Play Piano Note C
+        Play Chord Progression
       </button>
+      <button onClick={clickScale}>Play Scale</button>
     </div>
   );
 }
