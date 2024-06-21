f40fa1888a2d6df2db7c936b01690cb0eece7004
Josh Klope <klope3@msn.com>
2024-06-20 21:26:16 -0500

    Refactor simple piano example

--- src/app/InstrumentPlayer.tsx
+++ src/app/InstrumentPlayer.tsx
@@ -1,48 +1,41 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as Tone from "tone";
 
-const InstrumentPlayer: React.FC = () => {
+export function InstrumentPlayer() {
+  const [piano, setPiano] = useState(null as Tone.Sampler | null);
+
   useEffect(() => {
-    const startAudioContext = async () => {
+    async function startAudioContext() {
       await Tone.start();
       console.log("Audio context started");
-    };
-
+    }
     startAudioContext();
 
-    const piano = new Tone.Sampler({
-      urls: {
-        C4: "C4.mp3",
-      },
-      baseUrl: "https://tonejs.github.io/audio/salamander/",
-    }).toDestination();
-
-    const playNote = (instrument: string) => {
-      if (instrument === "piano") {
-        piano.triggerAttackRelease("C4", "4n");
-      }
-    };
-
-    const pianoButton = document.getElementById("playPianoC");
-
-    if (pianoButton) {
-      pianoButton.addEventListener("click", () => playNote("piano"));
-    }
-
-    return () => {
-      if (pianoButton) {
-        pianoButton.removeEventListener("click", () => playNote("piano"));
-      }
-    };
+    setPiano(
+      new Tone.Sampler({
+        urls: {
+          C4: "C4.mp3",
+        },
+        baseUrl: "https://tonejs.github.io/audio/salamander/",
+      }).toDestination()
+    );
   }, []);
+
+  function clickPiano() {
+    if (!piano) return;
+
+    piano.triggerAttackRelease("C4", "4n");
+  }
 
   return (
     <div>
-      <button id="playPianoC">Play Piano Note C</button>
+      <button onClick={clickPiano} id="playPianoC">
+        Play Piano Note C
+      </button>
     </div>
   );
-};
+}
 
 export default InstrumentPlayer;
